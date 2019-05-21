2c100276fbdf62ac4b735afc08530136eb897b08
Max Du <md@u12.co>
2019-05-21 16:09:22 +1000

    show, new, edit & update' '

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
